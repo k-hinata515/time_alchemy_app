fd63db604645bff2bd6192fa4ad6622dae07655d
sotatt <pg6eat5f@i.softbank.jp>
2023-11-21 11:59:47 +0900

    テスト

--- lib/View/start.dart
+++ lib/View/start.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(Start());
 }
-class Start extends StatelessWidget{
-  
-  @override
-  Widget build(BuildContext context){
-      return Scaffold(
-          
-      );
-  }
 
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        );
+  }
 }
