58bcca6ed477c95a74d5aa6142cacc6183c57fec
rituya1003 <2220029@ecc.ac.jp>
2023-11-21 09:26:15 +0900

    [fix] Firebaseの初期化がxcodeのバージョンで書き方変更されてたから修正

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,13 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+
+  // ここでFirebaseの初期化を行う前に、既存のFirebase Appが存在しないことを確認する
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp();
+  }
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
