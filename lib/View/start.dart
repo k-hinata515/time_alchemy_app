b6f7103f6042192fbaf28536ff081029490671b6
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2023-11-21 11:40:50 +0900

    　初コミット！

---
+++ lib/View/start.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Start());
+}
+class Start extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+          
+      );
+  }
+
+}
