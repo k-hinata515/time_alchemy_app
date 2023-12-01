8e9bc210d034d4531be49b5f07d1036771396ebb
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2023-12-01 16:47:10 +0900

    ボタンコンポーネントを作成しました

--- lib/component/background.dart
+++ lib/component/background.dart
@@ -1,1 +1,20 @@
-//test
+import 'package:flutter/material.dart';
+import 'package:time_alchemy_app/component/ButtonCompornent.dart';
+void main() {
+  runApp( Background());
+}
+class Background extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: ChoiceButton(
+        text:"次へ",
+        onPressed: (){
+          Navigator.of(context).pushNamed('time_alchemy_app/component/second.dart');
+        },
+        ),
+      )
+    );
+  }
+}
+
--- view/start.dart
+++ view/start.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Start());
-}
-class Start extends StatelessWidget{
+// void main() {
+//   runApp(Start());
+// // }
+// // class Start extends StatelessWidget{
   
-  @override
-  Widget build(BuildContext context){
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
+// //   @override
+// //   Widget build(BuildContext context){
+// //     double screenWidth = MediaQuery.of(context).size.width;
+// //     double screenHeight = MediaQuery.of(context).size.height;
     
-      return Column(
-        children: [
-          Container(
+// //       return Column(
+//         children: [
+//           Container(
 
-          )
-        ],
-      )
-  }
+// //           )
+// //         ],
+// //       )
+// //   }
 
-}
-//add
+// // // }
+// // // //add
