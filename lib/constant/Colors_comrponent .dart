623dedebeed5730e788763ad190ddcd814e2554a
sotatt <pg6eat5f@i.softbank.jp>
2023-12-12 09:38:29 +0900

    add

--- lib/constant/Colors_comrponent .dart
+++ lib/constant/Colors_comrponent .dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Colors_compornet {
-   static const globalBackgroundColorRed = Color(0x7E463E); //赤色
-   static const globalBackgroundColorwhite = Color(0xF2E9E2); //薄ピンク
-   static const inputFieldtextfontColor = Color(0xA98E87);
-   static const sortTagColor = Color(0xBD9F97);
-   static const textfontColorBlack = Color(0x333333);
-   static const textfontColorWhite = Color(0xFCFBFF);
+  static const globalBackgroundColorRed = Color(0xFF7E463E); //赤色
+  static const globalBackgroundColorwhite = Color(0xFFF2E9E2); //薄ピンク
+  static const inputFieldtextfontColor = Color(0xFFA98E87);
+  static const sortTagColor = Color(0xFFBD9F97);
+  static const textfontColorBlack = Color(0xFf333333);
+  static const textfontColorWhite = Color(0xFFFCFBFF);
+  static const borderColorGray = Color(0xFFD4C9C9);
 }
