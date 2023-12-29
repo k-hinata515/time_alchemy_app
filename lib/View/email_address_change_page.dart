716977bd6a45b4be3dabea216864c87f4fe13753
sotatt <pg6eat5f@i.softbank.jp>
2023-12-29 11:41:48 +0900

    add メールアドレス変更画面

--- lib/View/email_address_change_page.dart
+++ lib/View/email_address_change_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:time_alchemy_app/component/AppCompornent.dart';
+import 'package:time_alchemy_app/component/ButtonCompornent.dart';
+import 'package:time_alchemy_app/component/textformfield.dart';
 import 'package:time_alchemy_app/constant/Colors_comrponent%20.dart';
 import 'package:time_alchemy_app/screen_pod.dart';
 
@@ -27,14 +29,54 @@ class MyApp extends StatelessWidget {
 class EmailAddressChangePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final screen = ScreenRef(context).watch(screenProvider);
+    final screen = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors_compornet.globalBackgroundColorRed,
-      appBar: AppBarWhiteTextCompornent(
-          title: 'プロフィール',
-          rightText: '編集',
-          onPressedLeft: () => {},
-          onPressedRight: () => {}),
+      backgroundColor: Colors_compornet.globalBackgroundColorwhite,
+      appBar: AppBarBrackTextButtonCompornent(
+        leftText: 'キャンセル',
+        title: 'メールアドレス変更',
+        rightText: '完了',
+        onPressedLeft: () => {},
+        onPressedRight: () => {},
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: screen.height * 0.15,
+            ),
+            MailChangeFormField(
+              inputText: 'inputText',
+              hintText: '現在のメールアドレス',
+              height: screen.height * 0.05,
+              width: screen.width * 0.8,
+            ),
+
+            SizedBox(
+              height: screen.height * 0.02,
+            ),
+            MyTextFormField(
+              inputText: 'inputText',
+              hintText: '変更したいメールアドレス',
+              height: screen.height * 0.05,
+              width: screen.width * 0.8,
+            ),
+            SizedBox(
+              height: screen.height * 0.15,
+            ),
+            // 送信ボタン
+            ChoiceButtonRed(
+              text: '変更',
+              onPressed: () {
+                //TODO: メールアドレス変更処理を実行する
+              },
+              height: 100,
+              width: 10,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
