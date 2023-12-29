700c2bcb93d6397cb97a036e3e09aa91bc91a5f9
sotatt <pg6eat5f@i.softbank.jp>
2023-12-29 11:41:31 +0900

    update 現在のメールアドレス表示用の入力欄を追加した

--- lib/component/textformfield.dart
+++ lib/component/textformfield.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_alchemy_app/constant/screen_pod.dart';
+import 'package:time_alchemy_app/screen_pod.dart';
 
 class MyTextFormField extends StatelessWidget {
   final String hintText;
@@ -7,19 +7,30 @@ class MyTextFormField extends StatelessWidget {
   final double height;
   final double width;
 
-  MyTextFormField({required this.inputText, required this.hintText, required this.height,required this.width,});
+  MyTextFormField({
+    required this.inputText,
+    required this.hintText,
+    required this.height,
+    required this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
     final screen = ScreenRef(context).watch(screenProvider);
     return Container(
       height: screen.designH(height), // 高さの変更
-      width:screen.designW(width), // 横幅の変更
+      width: screen.designW(width), // 横幅の変更
       child: TextFormField(
         decoration: InputDecoration(
           hintText: hintText, //textfield内のラベル
-          hintStyle: const TextStyle(fontSize: 12, color: Color.fromRGBO(126, 70, 62, 1)),
-          fillColor: Color.fromRGBO(252, 251, 255, 1,), //textfeild内の色変更
+          hintStyle: const TextStyle(
+              fontSize: 12, color: Color.fromRGBO(126, 70, 62, 1)),
+          fillColor: Color.fromRGBO(
+            252,
+            251,
+            255,
+            1,
+          ), //textfeild内の色変更
           filled: true,
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20),
@@ -32,7 +43,62 @@ class MyTextFormField extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
             borderSide: BorderSide(
               color: Color.fromRGBO(126, 70, 62, 1),
-              width: 2.0,   //枠線の太さ変更
+              width: 2.0, //枠線の太さ変更
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// MailChangeFormField
+class MailChangeFormField extends StatelessWidget {
+  final String hintText;
+  final String inputText;
+  final double height;
+  final double width;
+  final TextEditingController controller; // 新しく追加
+
+  MailChangeFormField({
+    required this.inputText,
+    required this.hintText,
+    required this.height,
+    required this.width,
+    String initialValue = '', // 新しく追加
+  }) : controller = TextEditingController(text: initialValue); // 新しく追加
+
+  @override
+  Widget build(BuildContext context) {
+    final screen = ScreenRef(context).watch(screenProvider);
+    return Container(
+      height: screen.designH(height),
+      width: screen.designW(width),
+      child: TextFormField(
+        controller: controller, // 新しく追加
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(
+              fontSize: 12, color: Color.fromRGBO(126, 70, 62, 1)),
+          fillColor: Color.fromRGBO(
+            252,
+            251,
+            255,
+            1,
+          ),
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: const BorderSide(
+              color: Color.fromRGBO(126, 70, 62, 1),
+              width: 4.0,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(
+              color: Color.fromRGBO(126, 70, 62, 1),
+              width: 2.0,
             ),
           ),
         ),
