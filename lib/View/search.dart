2cc3a36b7c15aa4bacd893bfd2d64da1f062b331
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-01-18 09:44:52 +0900

    [update] コードを修正しました

--- lib/View/search.dart
+++ lib/View/search.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:device_preview/device_preview.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:time_alchemy_app/component/AppCompornent.dart';
 import 'package:time_alchemy_app/component/BackgroundCompornent.dart';
 import 'package:time_alchemy_app/component/ButtonCompornent.dart';
@@ -39,6 +43,7 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPage extends State<SearchPage> {
+  String now_time = DateFormat('HH:mm').format(DateTime.now()).toString();  //現在時刻
   final TextEditingController _next_destinationController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -58,63 +63,121 @@ class _SearchPage extends State<SearchPage> {
         body: Stack(
           children: [
             BackgroundWidget(),
-            Padding(
-              padding: EdgeInsets.only(top: screen.designH(100)),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+            Center(
+                child: Row(
                   children: [
-                    TextFormButton(
-                      height: screen.designH(45),
-                      width: screen.designW(200),
-                      labelText: '現在地',
-                      exampletext: '中崎町',
-                    ),
-                    SizedBox(height: screen.designH(16)),
-                    Container(
-                      height: screen.designH(200),
-                      width: screen.designW(280),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors_compornet.globalBackgroundColorRed, // 枠線の色
-                          width: 2.0, // 枠線の太さ
-                        ),
-                        borderRadius: BorderRadius.circular(20),
+                    SizedBox(width: screen.designW(16),),
+                    //現在時刻取得
+                    Align(
+                      alignment: Alignment(0,-0.63),
+                     child:Text(
+                      now_time,
+                      style: TextStyle(
+                        color: Colors_compornet.globalBackgroundColorRed,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14
                       ),
-                      // 他のウィジェットをここに追加
-                      child:  Column(
-                        children: [
-                          SizedBox(height: screen.designH(16),),
-                          MyTextFormField(
-                            obscuretext: false,
-                            labelText: '次の予定の目的地', 
-                            height: screen.designH(45), 
-                            width: screen.designW(200),
-                            controller:_next_destinationController, 
+                    ),
+                    ),
+                    //スペース
+                    SizedBox(width:screen.designW(16),),
+                    //時系列線
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Align(
+                          alignment: Alignment(0, -0.5),
+                          child: Dashed_Line(
+                            height: screen.designH(350),
+                            width: screen.designW(2), // Dashed_Line の幅を変更
                           ),
-                          SizedBox(height:screen.designH(16)),
-                          TextDisplay(
-                            labelText: '次の予定の到着時間', 
-                            height: screen.designH(45), 
-                            width: screen.designW(200),
-                            exampletext: '１３時'
+                        ),
+                        Align(
+                          alignment: Alignment(0, -0.65),
+                         child: Container(
+                            width: screen.designW(36),
+                            height: screen.designH(36),
+                            decoration: BoxDecoration(
+                              color: Colors_compornet.navigation_icon,
+                              shape: BoxShape.circle,
                             ),
-                        ]  
-                      ),
+                            child: Transform.rotate(
+                              angle: 180 * pi / 180, // 180°回転
+                              child: Icon(
+                                color:Colors_compornet.globalBackgroundColorRed,
+                                Icons.navigation,
+                               
+                              ),
+                            ),
+                          ), 
+                        )
+                        
+                      ],
                     ),
-                    SizedBox(height: screen.designH(112),),
-                    ChoiceButtonRed(
-                      height: screen.designH(45),
-                      width: screen.designW(250), 
-                      text: '検索', 
-                      onPressed: () {  
-                        print(_next_destinationController.text);
-                      },
-                    )
+
+                    
+                    //現在地
+                    Padding(
+                       padding: EdgeInsets.only(top: screen.designH(100)),
+                      child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextFormButton(
+                          height: screen.designH(45),
+                          width: screen.designW(200),
+                          labelText: '現在地',
+                          exampletext: '中崎町',
+                        ),
+                        SizedBox(height: screen.designH(16)),
+                        Container(
+                          height: screen.designH(200),
+                          width: screen.designW(280),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors_compornet.globalBackgroundColorRed, // 枠線の色
+                              width: 2.0, // 枠線の太さ
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          // 他のウィジェットをここに追加
+                          child:  Column(
+                            children: [
+                              SizedBox(height: screen.designH(16),),
+                              MyTextFormField(
+                                obscuretext: false,
+                                labelText: '次の予定の目的地', 
+                                height: screen.designH(45), 
+                                width: screen.designW(200),
+                                controller:_next_destinationController, 
+                              ),
+                              SizedBox(height:screen.designH(16)),
+                              TextDisplay(
+                                labelText: '次の予定の到着時間', 
+                                height: screen.designH(45), 
+                                width: screen.designW(200),
+                                exampletext: '１３時'
+                                ),
+                            ]  
+                          ),
+                          ),
+                        SizedBox(height: screen.designH(112),),
+                        ChoiceButtonRed(
+                          height: screen.designH(45),
+                          width: screen.designW(250), 
+                          text: '検索', 
+                          onPressed: () {  
+                            print(_next_destinationController.text);
+                          },
+                        )
+                      ],  
+                    ),
+                    ),           
+                    
                   ],
                 ),
+                
               ),
-            ),
+            
           ],
         ),
       ),
--- lib/constant/Colors_comrponent .dart
+++ lib/constant/Colors_comrponent .dart
@@ -7,11 +7,12 @@ class Colors_compornet {
   static const inputFieldtextfontColor = Color(0xFFA98E87); //textformfieldの内部の色に使用している
   static const sortTagColor = Color(0xFFBD9F97);    //setting_page.dartなどでのキャンセルボタンに使われている
   static const textfontcolorocher = Color(0xFFA98E87);  //MyTextFormFieldコンポーネントのlabelTextに使用
-  static const textfontColorBlack = Color(0xFf333333);  //黒色
-  static const textfontColorWhite = Color(0xFFFCFBFF);  //textの基本的ßな白に使用
+
   static const textfontColorGray = Color(0xFF7B7A7A); //Sign_up_Pageの「または」に使用
   static const borderColorGray = Color(0xFFD4C9C9);   //PickerCompornent.dartのドロップダウンリストに使われている？
   static const selectedtagdefault = Color(0xFFD9D9D9);  //Hobby_Registration.dartでの選択されていない時のタグに使用
   static const narrow_down = Color(0xFF654E48); //Add destination.dartでの絞り込みボタンに使用
-
+  static const navigation_icon = Color(0xFFEED8D2);//search.dartのNavigation iconで使用
+  static const textfontColorBlack = Color(0xFf333333);  //textの基本的な黒に使用
+  static const textfontColorWhite = Color(0xFFFCFBFF);  //textの基本的な白に使用
 }
