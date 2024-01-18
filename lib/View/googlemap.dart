5a0be5929655c498e571a34628a18fb2c7ec9dfd
rituya1003 <2220029@ecc.ac.jp>
2024-01-18 21:16:36 +0900

    [update]現在地の取得

--- lib/View/googlemap.dart
+++ lib/View/googlemap.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:time_alchemy_app/logic/flutter/googlemap_b.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,62 +12,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Google Maps Demo',
-      home: MapSample(),
+      home: MapScreen(),
     );
   }
 }
 
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
+// Googleマップを含むメイン画面のウィジェット
+class MapScreen extends StatelessWidget {
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
-  State<MapSample> createState() => MapSampleState();
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // 現在の位置を非同期で取得
+      future: MapLogic.getCurrentLocation(),
+      builder: (context, AsyncSnapshot<Position?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // 位置データを待っている間にローディングインジケータを表示
+          return CircularProgressIndicator();
+        } else {
+          // 位置データが利用可能になったらGoogleマップを表示
+          Position? initialPosition = snapshot.data;
+          return GoogleMapWidget(initialPosition: initialPosition);
+        }
+      },
+    );
+  }
 }
 
-class MapSampleState extends State<MapSample> {
-  Position? currentPosition;
-  late GoogleMapController _controller;
-  late StreamSubscription<Position> positionStream;
-  //初期位置
-  final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(43.0686606, 141.3485613),
-    zoom: 14,
-  );
+// Googleマップのためのウィジェット
+class GoogleMapWidget extends StatefulWidget {
+  final Position? initialPosition;
 
-  final LocationSettings locationSettings = const LocationSettings(
-    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
-    distanceFilter: 100,
-  );
+  const GoogleMapWidget({Key? key, this.initialPosition}) : super(key: key);
+
+  @override
+  State<GoogleMapWidget> createState() => GoogleMapWidgetState(initialPosition);
+}
+
+class GoogleMapWidgetState extends State<GoogleMapWidget> {
+  late GoogleMapController _controller;
+
+  // Googleマップの初期カメラ位置を現在位置に設定
+  CameraPosition get _initialCameraPosition {
+    return CameraPosition(
+      target: LatLng(
+        widget.initialPosition?.latitude ?? 0.0,
+        widget.initialPosition?.longitude ?? 0.0,
+      ),
+      zoom: 14,
+    );
+  }
+
+  Position? currentPosition;
+
+  // 初期位置で状態を初期化するためのコンストラクタ
+  GoogleMapWidgetState(Position? initialPosition) {
+    currentPosition = initialPosition;
+  }
 
   @override
   void initState() {
     super.initState();
-
-    //位置情報が許可されていない時に許可をリクエストする
-    Future(() async {
-      LocationPermission permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        await Geolocator.requestPermission();
-      }
-    });
-
-    //現在位置を更新し続ける
-    positionStream =
-        Geolocator.getPositionStream(locationSettings: locationSettings)
-            .listen((Position? position) {
-      currentPosition = position;
-      print(position == null
-          ? 'Unknown'
-          : '${position.latitude.toString()}, ${position.longitude.toString()}');
-    });
+    // 現在の位置を連続的に更新するための位置ストリームをセットアップ
+    LocationStream.getPositionStream(
+      const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      ),
+      (Position? position) {
+        setState(() {
+          currentPosition = position;
+          print(position == null
+              ? 'Unknown'
+              : '${position.latitude.toString()}, ${position.longitude.toString()}');
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      // Googleマップのプロパティを構成
       mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-      myLocationEnabled: true, //現在位置をマップ上に表示
+      initialCameraPosition: _initialCameraPosition,
+      myLocationEnabled: true,
       onMapCreated: (GoogleMapController controller) {
         _controller = controller;
       },
