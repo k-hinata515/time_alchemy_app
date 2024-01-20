4fa39e779087d43f4c4d6a916133955954021604
sotatt <pg6eat5f@i.softbank.jp>
2024-01-20 21:13:37 +0900

    update TextFormButtonの入力欄の色変更

--- lib/component/textformfield.dart
+++ lib/component/textformfield.dart
@@ -7,15 +7,19 @@ class MyTextFormField extends StatelessWidget {
   final String labelText;
   final double height;
   final double width;
-  final TextEditingController controller ;
+  final TextEditingController controller;
   final bool obscuretext;
-  MyTextFormField({required this.labelText, required this.height,required this.width,  required this.controller, required this.obscuretext});
+  MyTextFormField(
+      {required this.labelText,
+      required this.height,
+      required this.width,
+      required this.controller,
+      required this.obscuretext});
 
   @override
   Widget build(BuildContext context) {
     final screen = ScreenRef(context).watch(screenProvider);
     return Column(
-      
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
@@ -23,65 +27,71 @@ class MyTextFormField extends StatelessWidget {
           style: TextStyle(
             fontSize: 12,
             color: Colors_compornet.textfontcolorocher,
-            ),
+          ),
         ),
-        SizedBox(height: screen.designH(2),),
-        Container(      
+        SizedBox(
+          height: screen.designH(2),
+        ),
+        Container(
           height: screen.designH(height), // 高さの変更
-          width:screen.designW(width), // 横幅の変更
+          width: screen.designW(width), // 横幅の変更
           child: TextFormField(
             controller: controller,
             obscureText: obscuretext,
             decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors_compornet.globalBackgroundColorwhite,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors_compornet.globalBackgroundColorRed,
-                  width: 2.0  //太さ
-                ),
-                borderRadius: BorderRadius.circular(40) //外側の丸み
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide:  BorderSide(
-                  color:  Colors_compornet.globalBackgroundColorRed, width: 2,
-                ),
-                borderRadius: BorderRadius.circular(40) 
-              )
-            ),
+                filled: true,
+                fillColor: Colors_compornet.globalBackgroundColorwhite,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors_compornet.globalBackgroundColorRed,
+                        width: 2.0 //太さ
+                        ),
+                    borderRadius: BorderRadius.circular(40) //外側の丸み
+                    ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors_compornet.globalBackgroundColorRed,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(40))),
           ),
         ),
       ],
     );
   }
 }
-class SearchTextField extends StatelessWidget{
+
+class SearchTextField extends StatelessWidget {
   final String hintText;
   final double height;
   final double width;
-  final TextEditingController controller ;
+  final TextEditingController controller;
   final Function(String) onEditingComplete;
-  SearchTextField({required this.hintText, required this.height,required this.width,  required this.controller, required this.onEditingComplete,});
+  SearchTextField({
+    required this.hintText,
+    required this.height,
+    required this.width,
+    required this.controller,
+    required this.onEditingComplete,
+  });
   @override
   Widget build(BuildContext context) {
     return Container(
       height: height,
       width: width,
       child: TextField(
-        
         controller: controller,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.only(bottom: 8), //入力されたTextの位置の設定
-          fillColor: Colors_compornet.globalBackgroundColorwhite,
-          filled: true,
-          prefixIcon: Icon(
-            Icons.search,   //先頭のアイコン設定
-          ),
-          hintText: hintText, 
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-          )
-        ),
+            contentPadding: EdgeInsets.only(bottom: 8), //入力されたTextの位置の設定
+            fillColor: Colors_compornet.globalBackgroundColorwhite,
+            filled: true,
+            prefixIcon: Icon(
+              Icons.search, //先頭のアイコン設定
+            ),
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            )),
         onEditingComplete: () {
           onEditingComplete(controller.text); // 入力されたテキストを指定された関数に渡す
           FocusScope.of(context).unfocus(); // キーボードを閉じる
@@ -89,11 +99,7 @@ class SearchTextField extends StatelessWidget{
       ),
     );
   }
-  
 }
-
-
-
 
 class List_Display extends StatelessWidget {
   final String labelText;
@@ -136,28 +142,29 @@ class List_Display extends StatelessWidget {
           padding: EdgeInsets.all(10), // 必要に応じてパディングを追加
           child: Center(
             child: ListView.builder(
-            itemCount: (stringList.length / 3).ceil(), // 3つずつの列数
-            itemBuilder: (BuildContext context, int rowIndex) {
-              return Row(
-                children: List.generate(3, (int index) {
-                  final itemIndex = rowIndex * 3 + index;
-                  if (itemIndex < stringList.length) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 10), // アイテム間のスペーシング
-                      child: Text(
-                        '・${stringList[itemIndex]}', // 中黒を付ける
-                        style: TextStyle(
-                          color: Colors_compornet.textfontColorBlack,
+              itemCount: (stringList.length / 3).ceil(), // 3つずつの列数
+              itemBuilder: (BuildContext context, int rowIndex) {
+                return Row(
+                  children: List.generate(3, (int index) {
+                    final itemIndex = rowIndex * 3 + index;
+                    if (itemIndex < stringList.length) {
+                      return Padding(
+                        padding:
+                            const EdgeInsets.only(right: 10), // アイテム間のスペーシング
+                        child: Text(
+                          '・${stringList[itemIndex]}', // 中黒を付ける
+                          style: TextStyle(
+                            color: Colors_compornet.textfontColorBlack,
+                          ),
                         ),
-                      ),
-                    );
-                  } else {
-                    return Container(); // 空のコンテナを追加
-                  }
-                }),
-              );
-            },
-          ),
+                      );
+                    } else {
+                      return Container(); // 空のコンテナを追加
+                    }
+                  }),
+                );
+              },
+            ),
           ),
         ),
       ],
@@ -172,7 +179,7 @@ class String_Display extends StatelessWidget {
 
   String_Display({
     required this.labelText,
-    required this.displayText, 
+    required this.displayText,
   });
 
   @override
@@ -215,6 +222,7 @@ class String_Display extends StatelessWidget {
     );
   }
 }
+
 class TextFormButton extends StatelessWidget {
   final String labelText;
   final double height;
@@ -252,7 +260,7 @@ class TextFormButton extends StatelessWidget {
                 // ボタンが押された時の処理
               },
               style: ElevatedButton.styleFrom(
-                primary: Colors_compornet.globalBackgroundColorwhite,
+                primary: Colors_compornet.textEntryField,
                 onPrimary: Colors_compornet.globalBackgroundColorRed,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(40),
