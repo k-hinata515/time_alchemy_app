3841026a74ee5e5ff9fdb7102759f9e5e5a6d849
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-01-22 23:15:13 +0900

    [add]コードを更新しました

--- lib/View/googlemap.dart
+++ lib/View/googlemap.dart
@@ -1,16 +1,28 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:time_alchemy_app/constant/Colors_comrponent%20.dart';
+import 'package:time_alchemy_app/constant/screen_pod.dart';
 import 'package:time_alchemy_app/logic/flutter/googlemap_b.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp()), // Wrap your app
+    
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Google Maps Demo',
       home: MapScreen(),
     );
@@ -52,6 +64,21 @@ class GoogleMapWidget extends StatefulWidget {
 
 class GoogleMapWidgetState extends State<GoogleMapWidget> {
   late GoogleMapController _controller;
+  final data = [
+    'assets/logo_images/ECCcanpas.png',
+    'assets/logo_images/daisensou.png',
+    'assets/logo_images/osakastation.png',
+    'assets/logo_images/rinkutownstation.png',
+    'assets/logo_images/mcdnald.png'
+  ];
+  final label = [
+    
+    'ECCコンピュータ専門学校',
+    'ラーメン大戦争',
+    '大阪駅',
+    'りんくうタウン駅',
+    'マクドナルド'
+  ];
 
   // Googleマップの初期カメラ位置を現在位置に設定
   CameraPosition get _initialCameraPosition {
@@ -93,14 +120,128 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      // Googleマップのプロパティを構成
-      mapType: MapType.normal,
-      initialCameraPosition: _initialCameraPosition,
-      myLocationEnabled: true,
-      onMapCreated: (GoogleMapController controller) {
-        _controller = controller;
-      },
-    );
+    final screen = ScreenRef(context).watch(screenProvider);
+    return Scaffold(
+      backgroundColor: Colors_compornet.globalBackgroundColorRed,
+  body: Stack(
+    children: [
+      GoogleMap(
+        // Googleマップのプロパティを構成
+        mapType: MapType.normal,
+        initialCameraPosition: _initialCameraPosition,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+      ),
+      Padding(
+        padding: EdgeInsets.only(bottom: screen.designH(650)),
+        child: Column(
+          children: [
+          Container(
+            height:screen.designH(40),
+            width: double.infinity,
+            color: Colors_compornet.globalBackgroundColorRed,
+          ),
+          Container(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  width: screen.designW(1),
+                ),
+                Container(
+                  height: screen.designH(95),
+                  decoration: BoxDecoration(
+                    color: Colors_compornet.textfontColorWhite,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(width: screen.designW(8)),
+                      Container(
+                        height: screen.designH(45),
+                        width: screen.designW(75),
+                        decoration: BoxDecoration(
+                          color: Colors_compornet.globalBackgroundColorRed,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '現在地',
+                            style: TextStyle(
+                              color: Colors_compornet.globalBackgroundColorwhite,
+                            ),
+                          ),
+                        ),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            margin: EdgeInsets.only(left: 8),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  height: screen.designH(3),
+                                  width: screen.designW(70),
+                                  color: Colors_compornet.globalBackgroundColorRed,
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors_compornet.globalBackgroundColorRed,
+                                ),
+                                Container(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        width: screen.designW(60),
+                                        height: screen.designH(50),
+                                        child: FittedBox(
+                                          fit: BoxFit.contain,
+                                          child: Image.asset(
+                                            data[index],
+                                          ),
+                                        ),
+                                      ),
+                                      Text(
+                                        label[index],
+                                        style: TextStyle(
+                                          color: Colors_compornet.textfontcolorocher,
+                                          fontSize: label[index].length > 7
+                                              ? screen.designH(10)
+                                              : screen.designH(12),
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(width: screen.designW(8)),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+          ],
+        )
+      ),
+    ],
+  ),
+);
+
   }
 }
+
