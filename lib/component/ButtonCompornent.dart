d28b48f39c2eff967b28e8057c77dbaabb63ff9f
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-01-31 16:39:21 +0900

    [update]コードを修正しました

--- lib/component/ButtonCompornent.dart
+++ lib/component/ButtonCompornent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_alchemy_app/constant/Colors_comrponent%20.dart';
 import 'package:time_alchemy_app/constant/screen_pod.dart';
 
 class ChoiceButtonRed extends StatelessWidget {
@@ -14,35 +15,32 @@ class ChoiceButtonRed extends StatelessWidget {
     required this.height,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    final screen = ScreenRef(context).watch(screenProvider);
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Color.fromRGBO(
-            252,
-            251,
-            255,
-            1,
+    @override
+      Widget build(BuildContext context){
+        final screen = ScreenRef(context).watch(screenProvider);
+        return SizedBox(
+          height: screen.designH(height),
+          width: screen.designW(width),
+          child: ElevatedButton(
+          onPressed:onPressed,
+          child: Text(
+            text,
+            style: TextStyle(
+              color:Colors_compornet.textfontColorWhite,
+              fontSize: screen.designW(15),
+              
+            ),
+            ),
+          style: ElevatedButton.styleFrom(
+            elevation:20,
+            backgroundColor: Colors_compornet.globalBackgroundColorRed,
+            shape: RoundedRectangleBorder(
+              borderRadius:  BorderRadius.circular(40),
+            ),
           ),
-          fontSize: 20,
+          
         ),
-      ),
-      style: ElevatedButton.styleFrom(
-        elevation: 20,
-        fixedSize: Size(
-          screen.designW(width),
-          screen.designH(height),
-        ),
-        backgroundColor: Color.fromRGBO(126, 70, 62, 1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
-      ),
-    );
+      );
   }
 }
 
@@ -61,28 +59,26 @@ class ChoiceButtonWhite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screen = ScreenRef(context).watch(screenProvider);
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Color.fromRGBO(51, 51, 51, 1),
-          fontSize: 20,
-        ),
-      ),
-      style: ElevatedButton.styleFrom(
-        elevation: 20,
-        fixedSize: Size(screen.designH(height), screen.designW(width)),
-        backgroundColor: Color.fromRGBO(
-          252,
-          251,
-          255,
-          1,
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-      ),
-    );
+    return SizedBox(
+          height: screen.designH(height),
+          width: screen.designW(width),
+          child: ElevatedButton(
+          onPressed:onPressed,
+          child: Text(
+            text,
+            style: TextStyle(
+              color:Colors_compornet.textfontColorBlack,
+              fontSize: screen.designW(15),
+            ),
+            ),
+          style: ElevatedButton.styleFrom(
+            elevation:20,
+            backgroundColor: Colors_compornet.textfontColorWhite,
+            shape: RoundedRectangleBorder(
+              borderRadius:  BorderRadius.circular(40),
+            ),
+          ),
+        )
+        );
   }
 }
