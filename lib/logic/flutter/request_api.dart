d6cbe23d6b05df75cb9a91df3989751047987fb6
AluFu <0hp3261131m102u@ezweb.ne.jp>
2024-02-07 02:53:37 +0900

    [update]directionsRequest関数で 移動時間ではなく場所名、到着、出発時刻、滞在時間の配列を返すようにした

--- lib/logic/flutter/request_api.dart
+++ lib/logic/flutter/request_api.dart
@@ -1,21 +1,25 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:time_alchemy_app/logic/flutter/get_user_hobby.dart';
+import 'package:time_alchemy_app/logic/flutter/map_class.dart';
+import 'package:time_alchemy_app/logic/flutter/time_conversion.dart';
 
 class Request_API {
   Map<String, dynamic> _placesResponse = {};  // Places API のレスポンスを格納する変数
   Map<String, dynamic> _rootResponse = {};  // Places API のレスポンスを格納する変数
 
   List<String> _travel_time_list = [];  // 移動時間を格納するリスト
+  List<Map<String, String?>> time_list = [];  // 出発、到着、平均滞在時刻を格納するリスト
+  List<Map<String, String?>> Navigation_List = [];  // 経由地の名、到着、出発時刻を格納するリスト
   List<String> _user_hobby_List = [];  // ユーザーの趣味を格納する変数
   List<String> _hobby_place_name = [];  // ユーザーの趣味に関連する場所を格納する変数
-
+  
   // Places API (nearbySearch)にリクエストするための関数
   Future<Map<String, dynamic>> nearbySearchRequest(String latitude , String longitude) async {
     try {
       // Places API (nearbySearch) にリクエスト
       final http.Response response = await http.get(Uri.parse(
-          'http://10.200.0.210:5000/current_nearbysearch?latitude=$latitude&longitude=$longitude'));
+          'http://IP:Port/current_nearbysearch?latitude=$latitude&longitude=$longitude'));
 
       // 取得したデータを _placesResponse に代入
       _placesResponse = json.decode(response.body);
@@ -32,7 +36,7 @@ class Request_API {
     try {
       // Places API (textSearch) にリクエスト
       final http.Response response = await http.get(Uri.parse(
-          'http://10.200.0.210:5000/current_textsearch?textQuery=$text&latitude=$latitude&longitude=$longitude'));
+          'http://IP:Port/current_textsearch?textQuery=$text&latitude=$latitude&longitude=$longitude'));
 
       // 取得したデータを _placesResponse に代入
       _placesResponse = json.decode(response.body);
@@ -45,11 +49,10 @@ class Request_API {
 
 
     // Directions APIにリクエストするための関数
-  Future<List<String>> directionsRequest(
+  Future<List<Map<String, String?>>> directionsRequest(
     String latitude, 
     String longitude, 
-    String next_appointment_latitude, 
-    String next_appointment_longitude,
+    MapData next_appointment_data, 
     List<String> waypoints_list,
     DateTime next_appointment_time
   ) async {
@@ -59,7 +62,7 @@ class Request_API {
       
       // Directions API にリクエスト
       final http.Response directionsResponse = await http.get(Uri.parse(
-          'http://10.200.0.210:5000/current_places_root?origin=$latitude,$longitude&destination=$next_appointment_latitude,$next_appointment_longitude&waypoints=$waypoints_list&arrival_time=$arrival_time_UTC'));
+          'http://IP:Port/current_places_root?origin=$latitude,$longitude&destination=${next_appointment_data.latitude},${next_appointment_data.longitude}&waypoints=$waypoints_list&arrival_time=$arrival_time_UTC'));
 
       // 取得したデータを _rootResponse に代入
       _rootResponse = json.decode(directionsResponse.body);
@@ -67,23 +70,50 @@ class Request_API {
       // 取得した移動時間を_travel_time_listに格納
       for (int i = 0; i < _rootResponse['routes'][0]['legs'].length; i++) {
         _travel_time_list.add(_rootResponse['routes'][0]['legs'][i]['duration']['text']);
-      }      
+      }
+
+      // //TODO:travel_time_Listが取得できるまで待機 (test)
+      while (_travel_time_list.isEmpty) {
+        await Future.delayed(const Duration(milliseconds: 100));
+      }
+
+      //出発、到着、平均滞在時刻を取得
+      time_list = await Time_Conversion().convertTime(
+          DateTime.now(), _travel_time_list, next_appointment_time);
+
+      // Navigation_Listに場所名、到着、出発時刻、滞在時間を格納
+      for (int i = 0; i <= time_list.length - 1; i++) {
+        if (i == time_list.length - 1) {
+          Navigation_List.add({
+            'name': next_appointment_data.placeName,
+            'arrival_time': time_list[i]['arrival_time'],
+            'departure_time': time_list[i]['departure_time'],
+            'average_stay_time': time_list[i]['average_stay_time'],
+          });
+        } else {
+          Navigation_List.add({
+            'name': waypoints_list[i],
+            'arrival_time': time_list[i]['arrival_time'],
+            'departure_time': time_list[i]['departure_time'],
+            'average_stay_time': time_list[i]['average_stay_time'],
+          });
+        }
+      }
     } catch (error) {
         print(error);
     }
-    return _travel_time_list;
+    return Navigation_List;      
   }
 
   //OpenAI APIにリクエストするための関数
   Future<List<String>> openAIRequest() async {
     try {
-      print('リクエストされた');
       // _user_hobby_List = Get_User_Hobby().getUserHobbyData() as List<String>;
       //TODO:test
       _user_hobby_List = ['ファッション'];
       // OpenAI API にリクエスト
       final http.Response openAIResponse = await http.get(Uri.parse(
-          'http://IP/current_openAI?hobby=$_user_hobby_List'));
+          'http://IP:Port/current_openAI?hobby=$_user_hobby_List'));
       //ResponseStrにリクエスト結果を格納
       String ResponseStr = json.decode(openAIResponse.body);
       //ResponseStrをMap型に変換
@@ -92,7 +122,6 @@ class Request_API {
       // 取得したデータを _hobby_List に代入
       _hobby_place_name = List<String>.from(ResponseMap['hobby_places']);
 
-      print('受け取った');
     } catch (error) {
         print(error);
     }
